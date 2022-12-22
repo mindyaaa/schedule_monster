968c547e9e705f7621b7df307d37e0ed13f6f44d
mindyaaa <107449479+mindyaaa@users.noreply.github.com>
2022-12-22 22:09:13 +0900

    useQuery 오류 수정

--- src/pages/CharactersList.jsx
+++ src/pages/CharactersList.jsx
@@ -1,25 +1,29 @@
 import React, { useState } from 'react';
-import axios from 'axios'
+import axios from 'axios';
 import { ContentsBox, CharacterContainer, CharacterBox } from '../components/StoreStyle';
 import { useQuery } from '@tanstack/react-query';
 
+
 export default function CharactersList() {
 
-    const {isLoading, error, data} = useQuery(
-        ['pokemon'], async () => {
-            return axios.get('/pokemons/pokemon.json')
-            .then((pokemon) => console.log(pokemon.data))
+    const { isLoading, error, data } = useQuery(['pokemons'],
+     async () => {
+            const res = await axios.get('/pokemons/pokemon.json');
+            console.log(res.data);
+            return res.data.pokedata;
         }
-    )
+    );
     
     return (
         <ContentsBox>
+
             <CharacterContainer>
                 {isLoading &&
                 <h1 style={{textAlign:'center', alignSelf:'center', margin: '0 auto'}}>Loading,,,</h1> }
                 {error && <h1 style={{textAlign:'center', alignSelf:'center', margin: '0 auto'}}>Error 발생🤕</h1>}
-                {data && <CharacterBox></CharacterBox>}
+                {data && <CharacterBox>dd</CharacterBox>}
             </CharacterContainer>
+
         </ContentsBox>
     );
 }
